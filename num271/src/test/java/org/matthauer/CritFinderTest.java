bd488b21db57ac787ac3fbfbd068f58b078f6454
Matt Hauer <matthew.hauer@gmail.com>
2016-06-14 10:01:29 -0700

    Testing on the input values.  Could've used an input array, but eh.

--- num271/src/test/java/org/matthauer/CritFinderTest.java
+++ num271/src/test/java/org/matthauer/CritFinderTest.java
@@ -10,6 +10,7 @@ import junit.framework.TestSuite;
 public class CritFinderTest
     extends TestCase
 {
+    CritFinder cf;
     /**
      * Create the test case
      *
@@ -18,6 +19,7 @@ public class CritFinderTest
     public CritFinderTest(String testName )
     {
         super( testName );
+        cf = new CritFinder();
     }
 
     /**
@@ -28,12 +30,29 @@ public class CritFinderTest
         return new TestSuite( CritFinderTest.class );
     }
 
+    private double runCheck(int a, int b){
+        double rv = 0.0;
+        try {
+            rv = cf.findOneShotChance(a, b);
+        } catch(Exception e){
+
+        }
+        return rv;
+    }
     /**
      * Rigourous Test :-)
      */
     public void testApp()
     {
         assertTrue( true );
-        
+
+        assertEquals("1 on 4 failed", 1.0, runCheck(4, 1), 0.0001);
+        assertEquals("4 on 4 failed", 0.25, runCheck(4, 4), 0.0001);
+        assertEquals("5 on 4 failed", 0.25, runCheck(4, 5), 0.0001);
+        assertEquals("6 on 4 failed", 0.1875, runCheck(4, 6), 0.0001);
+        assertEquals("10 on 1 failed", 1.0, runCheck(1, 10), 0.0001);
+        assertEquals("200 on 100 failed", 0.0001, runCheck(100, 200), 0.0001);
+        assertEquals("20 on 8 failed", 0.009765625, runCheck(8, 20), 0.0001);
+
     }
 }
