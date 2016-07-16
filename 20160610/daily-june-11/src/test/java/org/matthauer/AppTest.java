420e1c441a7be90ea7e17e4f4a22380191ff28c0
Matt Hauer <matthew.hauer@gmail.com>
2016-07-16 06:31:01 -0700

    [ex270] Stubbed out some integrated tests to fill out later. Added JUnit 4 to the project.

--- 20160610/daily-june-11/src/test/java/org/matthauer/AppTest.java
+++ 20160610/daily-june-11/src/test/java/org/matthauer/AppTest.java
@@ -1,38 +1,42 @@
 package org.matthauer;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
     /**
      * Create the test case
      *
-     * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public AppTest()
     {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testFive(){
+        int result = 0;
+        assertEquals("five grid result incorrect", result, 9);
+    }
+
+    @Test
+    public void testEight(){
+        int result = 0;
+        assertEquals("eight grid result incorrect", result, 16);
     }
 }
